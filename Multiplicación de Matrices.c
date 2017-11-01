c72cc71b26d61a33df67804b0f1e5993f10e62df
JyuHysteria <31145605+JyuHysteria@users.noreply.github.com>
2017-11-01 10:45:34 -0600

    Update Multiplicación de Matrices.c

--- Multiplicación de Matrices.c
+++ Multiplicación de Matrices.c
@@ -5,11 +5,11 @@ int main()
   int main i,j,k,l,m,n,f;
   float M1[3][3], M2[3][3], M3[3][3];
   printf("\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n");
-  printf("\ INGRESE LA FILA Y COLUMNA DE LA PRIMERA MATRIZ\n");
+  printf("\nINGRESE LA FILA Y COLUMNA DE LA PRIMERA MATRIZ\n");
   scantf("%d", &i);
   scantf("%d", &j);
   printf("\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n");
-  printf("\ INGRESE LA FILA Y COLUMNA DE LA SEGUNDA MATRIZ\n");
+  printf("\nINGRESE LA FILA Y COLUMNA DE LA SEGUNDA MATRIZ\n");
   scantf("%d", &k);
   scantf("%d", &l);
   printf("\n");
