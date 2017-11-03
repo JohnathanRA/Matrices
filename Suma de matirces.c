2adf51a6999edeb788b53046efb6270ef72b5ed1
AlexisOC <31287735+AlexisOC@users.noreply.github.com>
2017-11-03 17:32:21 -0600

    Update Suma de matirces.c

--- Suma de matirces.c
+++ Suma de matirces.c
@@ -3,23 +3,42 @@ int main(){
   int M1[3][3]={{1,2,3},{3,2,1},{1,2,3}};
   int M2[3][3]={{1,2,3},{3,2,1},{1,2,3}};
   int M3[3][3]={{},{},{}};
-  int i, j, t=0, *p;
-  *p=&t;
+  int i, j, t=0;
   for(i=0;i<3;i++){
     for(j=0;j<3;j++){
-      printf("Dame 1 numero para la primer matriz\n");
-      scanf("%d", & M1[i][j]);
-      printf("Dame 1 numero para la segunda matriz\n");
-      scanf("%d", & M1[i][j]);
+      printf("Da 2 numeros para operar matrices\n");
+      scanf("%d %d", & M1[i][j],&M2[i][j]);
     }
   }
-  for(i=0;i<3;i++){
-    for(j=0;j<3;j++){
-      *p=M1[i][j]+M2[i][j];
-      M3[i][j]=*p;
-      printf(" %d", M3[i][j]);
-      *p=0;
-    }
-    printf("\n");
+for(i=0;i<3;i++){
+  	for(j=0;j<3;j++){
+  		printf("%d ",M1[i][j]);
+  	}
+  	if(i==1){
+	  
+  	printf("+ ");
+  }else{
+  	printf("\t");
+  }
+  	for(j=0;j<3;j++){
+  		printf("%d ",M2[i][j]);
+	}
+	if(i==1){
+	  
+  	printf("= ");
+  }else{
+  	printf(" \t");
+  }
+	for(j=0;j<3;j++){
+      	t=M1[i][j]0*M2[i][j];
+      	M3[i][j]=t;
+      	if(M3[i][j]>9){
+      		printf("%d ", M3[i][j]);
+		  }else{
+		  printf("%d  ", M3[i][j]);
+	}
+		t=0;
+    	}
+	printf("\n");
   }
 }
