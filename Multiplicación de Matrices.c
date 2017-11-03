2db9f8384e0afbbef997e7fc0d0b12df0a0a0753
AlexisOC <31287735+AlexisOC@users.noreply.github.com>
2017-11-03 17:50:54 -0600

    Update Multiplicación de Matrices.c

--- Multiplicación de Matrices.c
+++ Multiplicación de Matrices.c
@@ -1,20 +1,3 @@
-#include<stedio.h>
-
-int main()
-{
-  int main i,j,k,l,m,n,f;
-  float M1[3][3], M2[3][3], M3[3][3];
-  printf("\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n");
-  printf("\nINGRESE LA FILA Y COLUMNA DE LA PRIMERA MATRIZ\n");
-  scantf("%d", &i);
-  scantf("%d", &j);
-  printf("\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n");
-  printf("\nINGRESE LA FILA Y COLUMNA DE LA SEGUNDA MATRIZ\n");
-  scantf("%d", &k);
-  scantf("%d", &l);
-  printf("\n");
-  if (i==k)
-    
 #include<stdio.h>
 int main(){
 	int M1[3][3]={{},{},{}};
@@ -30,6 +13,7 @@ int main(){
 				case 0:
 					printf("Dame el primer valor de la fila %d de la primera y segunda matriz:\n", i+1);
 					scanf("%d %d", & M1[i][j], & M2[i][j]);
+					
           				break;
 				case 1:
 					printf("Dame el segundo valor de la fila %d de la primera y segunda matriz:\n", i+1);
@@ -42,13 +26,37 @@ int main(){
 			}
 		}
 	}
-  for(i=0;i<3;i++){
+for(i=0;i<3;i++){
+  	for(j=0;j<3;j++){
+  		printf("%d ",M1[i][j]);
+  	}
+  	if(i==1){
+	  
+  	printf("x ");
+  }else{
+  	printf("\t");
+  }
+  	for(j=0;j<3;j++){
+  		printf("%d ",M2[i][j]);
+	}
+	if(i==1){
+	  
+  	printf("= ");
+  }else{
+  	printf(" \t");
+  }	
     for(j=0;j<3;j++){
       *p=M1[i][j]*M2[i][j];
       M3[i][j]=*p;
-      printf(" %d", M3[i][j]);
+      if(M3[i][j]>9){
+      		printf("%d ", M3[i][j]);
+		  }else{
+		  printf("%d  ", M3[i][j]);
+	}
       *p=0;
     }
     printf("\n");
+
   }
 }
+
